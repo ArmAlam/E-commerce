47b824b488812656f045119d4ee88fd916dec412
ArmanCSE <arman.u.alam@gmail.com>
2020-04-13 12:07:51 +0600

    On Page SEO Done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,56 +1,58 @@
 @extends('layouts.app')
-
 @section('content')
+@php 
+  $order = DB::table('orders')->where('user_id',Auth::id())->orderBy('id','DESC')->limit(10)->get();
+@endphp
+
     <div class="contact_form">
-        <div class="row">
-            <div class="col-8 card">
-                <table class="table table-response">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Xain</td>
-                            <td>Shujha</td>
-                            <td>@nbo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Leam</td>
-                            <td>Rado</td>
-                            <td>@Jim</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="col-4">
-                <div class="card" style="width: 18rem;">
-                <img src="{{ asset('public/avatar.jpg') }}"  class="card-img-top" style="height: 90px; width: 90px; margin-left: 34%;" >
-                <div class="card-body">
-                    <h5 class="card-title text-cetner"> {{ Auth::user()->name }}</h5>
+        <div class="container">
+            <div class="row">
+               <div class="col-8 card">
+                 <table class="table table-response">
+                   <thead>
+                     <tr>
+                       <th scope="col">PaymentType</th>
+                       <th scope="col">Payment ID</th>
+                       <th scope="col">Amount</th>
+                       <th scope="col">Date</th>
+                        <th scope="col">Status Code</th>
+                        <th scope="col">Action</th>
+                     </tr>
+                   </thead>
+                   <tbody>
+                    @foreach($order as $row)
+                     <tr>
+                       <th >{{ $row->payment_type }}</th>
+                       <td>{{ $row->payment_id }}</td>
+                       <td>{{ $row->total }} $</td>
+                       <td>{{ $row->date }}</td>
+                       <td>{{ $row->status_code }}</td>
+                       <td>
+                         <a href="#" class="btn btn-sm btn-info">View</a>
+                       </td>
+                     </tr>
+                    @endforeach
+                   </tbody>
+                 </table>
+               </div>
+               <div class="col-4">
+                 <div class="card" style="width: 18rem;">
+                  <img src="{{ asset('public/avatar.jpg') }}" class="card-img-top" style="height: 90px; width: 90px; margin-left: 34%;" >
+                  <div class="card-body">
+                    <h5 class="card-title text-center">{{ Auth::user()->name }}</h5>
+                  </div>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><a href="{{ route('password.change') }}"> Password Change </a></li>
+                    <li class="list-group-item"><a href="{{ route('password.change') }}"> Edit Profile </a></li>
+                    <li class="list-group-item"><a href="{{ route('password.change') }}"> Return Order </a></li>
+                  </ul>
+                  <div class="card-body">
+                    <a href="{{ route('user.logout') }}" class="btn btn-danger btn-sm btn-block">Logout</a>
+                  </div>
                 </div>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item"><a href="{{ route('password.change') }}">Password Change</a></li>
-                    <li class="list-group-item">ainmbv jkg</li>
-                    <li class="list-group-item">CrJFjffo Jacob</li>
-                </ul>
-                <div class="card-body">
-                <a href="{{ route('user.logout') }}" class="btn btn-danger btn-sm btn-block">Logout</a>
-                </div>
-                </div>
+               </div>
             </div>
         </div>
     </div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('user/checkout/', 'CartController@Checkout')->name('user.checkout');
 Route::get('user/wishlist/', 'CartController@Wishlist')->name('user.wishlist');
 Route::post('user/apply/coupon/', 'CartController@Coupon')->name('apply.coupon');
 Route::get('coupon/remove/', 'CartController@CouponRemove')->name('coupon.remove');
-Route::get('payment/page/', 'CartController@PaymentPage')->name('payment.step');
+Route::get('payment/page/', 'CartController@PymentPage')->name('payment.step');
 
 //payment methods
 Route::post('user/payment/process/', 'PaymentController@payment')->name('payment.process');
@@ -85,9 +85,12 @@ Route::post('admin/store/coupon', 'Admin\CouponController@StoreCoupon')->name('s
 Route::get('delete/coupon/{id}', 'Admin\CouponController@DeleteCoupon');
 Route::get('edit/coupon/{id}', 'Admin\CouponController@EditCoupon');
 Route::post('update/coupon/{id}', 'Admin\CouponController@UpdateCoupon');
+
 //newslater
-Route::get('admin/newslettter', 'Admin\CouponController@Newslater')->name('admin.newsletter');
+Route::get('admin/newslater', 'Admin\CouponController@Newslater')->name('admin.newslater');
 Route::get('delete/sub/{id}', 'Admin\CouponController@DeleteSub');
+Route::get('admin/seo', 'Admin\CouponController@Seo')->name('admin.seo');
+Route::post('admin/update/seo', 'Admin\CouponController@UpdateSeo')->name('update.seo');
 //products routes=====
 Route::get('admin/product/all', 'Admin\ProductController@index')->name('all.product');
 Route::get('admin/product/add', 'Admin\ProductController@create')->name('add.product');
@@ -111,7 +114,6 @@ Route::post('update/post/{id}', 'Admin\PostController@update');
 //admin order routes
 Route::get('admin/pending/order', 'Admin\OrderController@NewOrder')->name('admin.neworder');
 Route::get('admin/view/order/{id}', 'Admin\OrderController@ViewOrder');
-
 Route::get('admin/payment/accept/{id}', 'Admin\OrderController@PaymentAccept');
 Route::get('admin/payment/cancel/{id}', 'Admin\OrderController@PaymentCancel');
 Route::get('admin/accept/payment', 'Admin\OrderController@AcceptPaymentOrder')->name('admin.accept.payment');
@@ -121,14 +123,32 @@ Route::get('admin/cancel/payment', 'Admin\OrderController@CancelPaymentOrder')->
 Route::get('admin/delevery/progress/{id}', 'Admin\OrderController@DeleveryProgress');
 Route::get('admin/delevery/done/{id}', 'Admin\OrderController@DeleveryDone');
 
-
-
-
-//frontend all routes are here--------
+//orders routes
+Route::get('admin/today/order', 'Admin\ReportController@TodayOrder')->name('today.order');
+Route::get('admin/today/deleverd', 'Admin\ReportController@TodayDelevered')->name('today.delevered');
+Route::get('admin/this/month', 'Admin\ReportController@ThisMonth')->name('this.month');
+Route::get('admin/search/report', 'Admin\ReportController@search')->name('search.report');
+Route::post('admin/search/byyear', 'Admin\ReportController@searchByYear')->name('search.by.year');
+Route::post('admin/search/bymonth', 'Admin\ReportController@searchByMonth')->name('search.by.month');
+Route::post('admin/search/bydate', 'Admin\ReportController@searchByDate')->name('search.by.date');
+//user role
+Route::get('admin/create/role', 'Admin\ReportController@UserRole')->name('create.user.role');
+Route::get('admin/create/admin', 'Admin\ReportController@UserCreate')->name('create.admin');
+Route::post('admin/store/admin', 'Admin\ReportController@UserStore')->name('store.admin');
+Route::get('delete/admin/{id}', 'Admin\ReportController@UserDelete');
+Route::get('edit/admin/{id}', 'Admin\ReportController@UserEdit');
+Route::post('admin/update/admin', 'Admin\ReportController@UserUpdate')->name('update.admin');
+//site setting
+Route::get('admin/site/setting', 'Admin\SettingController@SiteSetting')->name('admin.site.setting');
+Route::post('admin/update/sitesetting', 'Admin\SettingController@UpdateSetting')->name('update.sitesetting');
+//frontedn all routesa are here--------
 Route::post('store/newslater', 'FrontController@StoreNewslater')->name('store.newslater');
 
 Route::get('/product/details/{id}/{product_name}', 'ProductController@ProductView');
 Route::post('/cart/product/add/{id}', 'ProductController@AddCart');
 
 Route::get('/products/{id}', 'ProductController@productsView');
+
+
+Route::post('order/tracking', 'FrontController@OrderTracking')->name('order.tracking');
 //customer profile related routes
