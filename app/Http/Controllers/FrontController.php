cff1c135adba967b8932934a74c88b0cc2f97807
ArmanCSE <arman.u.alam@gmail.com>
2020-04-16 08:58:12 +0600

    Product Search Added

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -39,4 +39,17 @@ class FrontController extends Controller
             return Redirect()->back()->with($notification);
         }
     }
+
+    public function ProductSearch(Request $request)
+    {
+        $brands = DB::table('brands')->get();
+        $item = $request->search;
+        $products = DB::table('products')
+            // ->join('brands','products.brand_id','brands.id')
+            // ->select('products.*','brands.brand_name')
+            ->where('product_name', 'LIKE', "%{$item}%")
+            // ->orWhere('brand_name','LIKE', "%{$item}%")
+            ->paginate(20);
+        return view('pages.search', compact('brands', 'products'));
+    }
 }
